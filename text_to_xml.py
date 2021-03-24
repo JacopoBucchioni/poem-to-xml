ee673192552ffdca77ff2eab2edf538d9443bf6d
JacopoBucchioni <bucchionjacopo96@gmail.com>
2021-03-24 17:48:33 +0100

    Update text_to_xml.py

--- text_to_xml.py
+++ text_to_xml.py
@@ -4,6 +4,8 @@ import xml.etree.ElementTree as ET
 
 supportedTextExtension = ['.txt']
 
+dict = {2: 'distico', 3: 'terzina', 4: 'quartina', 6: 'sestina', 8: 'ottava'}
+
 
 def dir_path(path):
     if os.path.exists(path):
@@ -37,21 +39,28 @@ def file_to_xml(filePath, encoding):
         lines = text.splitlines()
         file.close()
 
-        root = ET.Element("lg")
+        root = ET.Element("text")
         strofa = ET.SubElement(root, "lg")
+        count = 0
         for line in lines:
             if line:
                 l = ET.SubElement(strofa, "l")
                 l.text = line
+                count += 1
             else:
+                if count in dict:
+                    strofa.attrib['type'] = dict[count]
+                count = 0
                 strofa = ET.SubElement(root, "lg")
+        strofa.attrib['type'] = dict[count]
+
         xml = ET.ElementTree(root)
         ET.indent(xml)
 
         dir = os.path.dirname(p[0]) + '/xml'
         if not os.path.exists(dir):
             os.makedirs(dir)
-        xml.write(dir + '/' + os.path.basename(p[0]) + '.xml', encoding=encoding)
+        xml.write(dir + '/' + os.path.basename(p[0]) + '.xml', encoding=encoding, xml_declaration=True)
 
 
 if __name__ == '__main__':
@@ -59,4 +68,3 @@ if __name__ == '__main__':
         get_XMLs(args.path, args.encoding)
     else:
         get_XMLs(args.path)
-
