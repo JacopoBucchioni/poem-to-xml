cc1d828ba4abef219cffb2c8ecf01b057a9a6bfd
JacopoBucchioni <bucchionjacopo96@gmail.com>
2021-03-31 12:19:02 +0200

    Update poem-to-xml.py

--- poem-to-xml.py
+++ poem-to-xml.py
@@ -3,7 +3,6 @@ import argparse
 import xml.etree.ElementTree as ET
 from timeit import default_timer as timer
 
-
 supportedTextExtension = ['.txt']
 
 dict = {2: 'couplet', 3: 'triplet', 4: 'quatrain', 6: 'sestet', 8: 'octave'}
